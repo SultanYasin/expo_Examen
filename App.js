c1d77d1219e44534156288c1a8ba1a630978c939
Sultan Yasin <sultan.yasin@utb.ecutbildning.se>
2023-02-25 23:45:25 +0100

    adding exercises component

--- App.js
+++ App.js
@@ -1,22 +1,38 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 import HomeScreen from "./screen/HomeScreen";
 
 export default function App() {
 
-  const [textInputValue, setTextInputValue] = useState(second)
+  const [textInputValue, setTextInputValue] = useState("")
+  const [index, setIndex] = useState([])
 
   const handleTextChange = (text) => {
     setTextInputValue(text)
+    
   };
+
+  useEffect(() => {
+    console.log(index)
+  }, [index])
+  
+
+const handleAdd = () => {
+  setIndex(prev => prev.concat(textInputValue + " \n "))
+  setTextInputValue("")
+  
+}
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Add your daily exercise program</Text>
       <TextInput
-        onChangeText={(e) => e.target.value}
+        onChangeText={handleTextChange}
         style={styles.textInput}
+        value={textInputValue}
       />
+      <Button title="Add" onPress={handleAdd}/>
     </View>
   );
 }
