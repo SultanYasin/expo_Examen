9f7488ea0e1d0956d7d2c0cba8dd8f82ff6a6238
Sultan Yasin <sultan.yasin@utb.ecutbildning.se>
2023-02-26 23:04:57 +0100

    modals

---
+++ models/Todo.ts
@@ -0,0 +1,9 @@
+exprot default class Todo {
+    constructor(id , title ,number , times, isCompleted){
+        this.id = id, 
+        this.title = title,
+        this.number = number,
+        this.times = times,
+        this.isCompleted = isCompleted
+    }
+}
