65374a29d99584e6ca109903116198a141c57733
Sultan Yasin <sultan.yasin@utb.ecutbildning.se>
2023-02-26 22:42:29 +0100

    x

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { Button, FlatList, StyleSheet, Text, TextInput, View } from "react-native";
+import { Button, Dimensions, FlatList, ImageBackground, Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import HomeScreen from "./screen/HomeScreen";
 
 export default function App() {
@@ -25,23 +25,35 @@ const handleAdd = () => {
   
 }
 
-const _renderItems = ({item: exercise}) => {
-  return( <View>
+const handleChangeItem = () =>{
+  console.log("handleChange has been clicked on : " + new Date().toISOString().slice(0 , 10))
 
-   <span> <Text> {exercise} </Text></span>
+}
+
+const _renderItems = ({item: exercise}) => {
+  return( <View   >
+
+    <Text onPress={handleChangeItem} > {exercise} </Text>
+    <Text> {"______________________"} </Text>
   </View>
   )
 }
 
   return (
     <View style={styles.container}>
+      <ImageBackground source={require("./assets/2.jpg")} resizeMode="cover" style={styles.bgImg}  >
       <Text style={styles.title}>Add your daily exercise program</Text>
       <TextInput
         onChangeText={handleTextChange}
         style={styles.textInput}
         value={textInputValue}
       />
-      <Button title="Add" onPress={handleAdd}/>
+
+       <Pressable onPress={handleAdd}  style={({pressed}) => [styles.btn, {opacity: pressed ? 0.5 : 1.0 }] } >
+          <Text style={styles.btnTxt} >Add</Text>
+        </Pressable> 
+
+    
 
       <FlatList 
         data={exercises}
@@ -49,6 +61,7 @@ const _renderItems = ({item: exercise}) => {
         keyExtractor = {(exercise, index) => index}
         
       />
+      </ImageBackground>
     </View>
   );
 }
@@ -67,9 +80,29 @@ const styles = StyleSheet.create({
   },
   textInput: {
     backgroundColor: "gray",
-    width: "70%",
+    width: "100%",
     margin: 20,
     padding: 10,
     borderRadius: 5,
   },
+  bgImg: {
+    height: Dimensions.get("window").height,
+    width: Dimensions.get("window").width,
+  },
+  btn:{
+    color:"white",
+    backgroundColor: "dodgerblue",
+    marginHorizontal:10,    
+    borderRadius:8,  
+    height:40,
+    
+  },
+  btnTxt: {
+    fontSize:30,
+    color:"white",
+    textAlign:"center",
+
+  }
+
+
 });
